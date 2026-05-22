fe7cf07c949c68c26177b39eb33989c0be1b04b4
engouloudyllan <engouloudyllan117@gmail.com>
2026-05-22 19:15:19 +0200

    remove dist from repository

--- dist/src/controllers/user.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import express from "express";
-declare class UserController {
-    static getAll(req: express.Request, res: express.Response, next: express.NextFunction): Promise<express.Response<any, Record<string, any>> | undefined>;
-}
-export default UserController;
-//# sourceMappingURL=user.controller.d.ts.map
--- dist/src/models/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "@prisma/client";
-declare const prisma: PrismaClient<{
-    adapter: PrismaMariaDb;
-}, never, import("@prisma/client/runtime/client").DefaultArgs>;
-export { prisma };
-//# sourceMappingURL=index.d.ts.map
